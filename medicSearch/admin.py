960d6f4f43b515b9c100ce4617c65a4e5da5d08b
rogerio.martins <rogerio.martins@enap.gov.br>
2021-09-04 23:59:55 -0300

    Creating and customizing all models

---
+++ medicSearch/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Profile)
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(Speciality)
+admin.site.register(Rating)
+admin.site.register(DayWeek)
+admin.site.register(Address)
+admin.site.register(Neighborhood)
+
+
+
