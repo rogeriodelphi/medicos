c153329097982da805d10a0b3faef272451f6ec2
rogerio.martins <rogerio.martins@enap.gov.br>
2021-09-08 00:18:58 -0300

    Customizing the Admin

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Profile)
-admin.site.register(State)
-admin.site.register(City)
-admin.site.register(Speciality)
-admin.site.register(Rating)
-admin.site.register(DayWeek)
-admin.site.register(Address)
-admin.site.register(Neighborhood)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'birthday')
+    empty_value_display = 'Vazio'
+    list_display_links = ('user', 'role')
+    list_filter = ('user__is_active',)
+    fields = ('user', ('role', ), 'image', 'birthday',  'specialties', 'addresses',)
+    exclude = ('favorites', 'created_at', 'updated_at')
+    readonly_fields = ('user',)
+    search_fields = ('user__username',)
+    date_hierarchy = 'created_at'
 
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Address)
+admin.site.register(DayWeek)
+admin.site.register(Rating)
+admin.site.register(Speciality)
 
 
