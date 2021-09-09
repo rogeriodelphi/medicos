87555d190fe93f6291a44e5f8b25740dcef0985c
rogerio.martins <rogerio.martins@enap.gov.br>
2021-09-08 23:49:57 -0300

    Customizing the Admin

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -2,17 +2,42 @@ from django.contrib import admin
 from .models import *
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'role', 'birthday')
-    empty_value_display = 'Vazio'
+    list_display = ('user', 'role', 'birth', 'specialtiesList', 'addressesList',)
+    empty_value_display = '----'
     list_display_links = ('user', 'role')
-    list_filter = ('user__is_active',)
-    fields = ('user', ('role', ), 'image', 'birthday',  'specialties', 'addresses',)
+    list_filter = ('user__is_active', 'role')
+    # fields = ('user', ('role', ), 'image', 'birthday',  'specialties', 'addresses',)
     exclude = ('favorites', 'created_at', 'updated_at')
     readonly_fields = ('user',)
     search_fields = ('user__username',)
     date_hierarchy = 'created_at'
 
+    fieldsets = (
+        ('Usuário', {
+            'fields': ('user', 'birthday', 'image')
+        }),
+        ('Função', {
+            'fields': ('role',)
+        }),
+        ('Extras', {
+            'fields': ('specialties', 'addresses')
+        }),
+    )
+
+    def specialtiesList(self, obj):
+        return [i.name for i in obj.specialties.all()]
+
+    def addressesList(self, obj):
+        return [i.name for i in obj.addresses.all()]
+
+    def birth(self, obj):
+        if obj.birthday:
+            return obj.birthday.strftime("%d/%m/%Y")
+
+    birth.empty_value_display = '___/___/_____'
+
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Neighborhood)
 admin.site.register(Address)
 admin.site.register(DayWeek)
 admin.site.register(Rating)
